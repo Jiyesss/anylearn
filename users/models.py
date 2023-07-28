8a46871afe21dd304283f047b8aa85aba0a4747a
yuunseo <dbstj7457@naver.com>
2023-07-28 11:59:29 +0900

    #field내용 중 "editable"속성 수정

--- users/models.py
+++ users/models.py
@@ -3,11 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    name = models.CharField(
-        max_length=30,
+    # AbstractUser에서 받아온 필드 중 수정하고 싶지 않은 필드 지정하기
+    first_name = models.CharField(
+        max_length=150,
         editable=False,
     )
-
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    # 입력받고 싶은 필드들 작성하기
+    name = models.CharField(
+        max_length=30,
+    )
     avatar = models.URLField(
         blank=True,
     )
@@ -16,7 +24,6 @@ class User(AbstractUser):
     )
     phonenumber = models.CharField(
         max_length=20,
-        editable=False,
     )
     nickname = models.CharField(
         max_length=30,
