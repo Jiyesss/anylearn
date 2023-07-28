f529f084f638ccb3fa695c42444c9ee8d708d0c6
yuunseo <dbstj7457@naver.com>
2023-07-28 15:44:18 +0900

    #GET api/v1/scripts

--- scripts/views.py
+++ scripts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Script
+from .serializers import ScriptSerializer
+
 
 # Create your views here.
+class Scripts(APIView):
+    def get(self, requet):
+        all_scripts = Script.objects.all()
+        serializer = ScriptSerializer(
+            all_scripts,
+            many=True,
+        )
+        return Response(serializer.data)
