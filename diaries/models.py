19691068ddf6b753297b59f8574692732a0e2026
yuunseo <dbstj7457@naver.com>
2023-07-28 15:23:55 +0900

    #diary model -> comment필드 추가

--- diaries/models.py
+++ diaries/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Create your models here.
 class Diary(models.Model):
     nowDate = models.DateField()
+    comment = models.TextField(
+        null=True,
+    )
     diaryContents = models.ManyToManyField("scripts.Script")
     user_email = models.ForeignKey(
         "users.USer",
