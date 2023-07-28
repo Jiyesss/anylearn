e57b1da2e2c75760fdd20ca051e27aee52cae8dc
yuunseo <dbstj7457@naver.com>
2023-07-28 12:19:07 +0900

    #users admin화면- password추가 및 설명

--- users/admin.py
+++ users/admin.py
@@ -5,12 +5,14 @@ from .models import User
 
 @admin.register(User)
 class CustomerUserAdmin(UserAdmin):
+    # admin/Users에서 user를 선택했을 때 뜨는 화면
     fieldsets = (
         (
             "Profile",
             {
                 "fields": (
                     "name",
+                    "password",
                     "avatar",
                     "birth",
                     "phonenumber",
@@ -20,9 +22,10 @@ class CustomerUserAdmin(UserAdmin):
             },
         ),
     )
-
+    # admin/Users에서 보이는 화면
     list_display = [
         "username",
+        "password",
         "avatar",
         "birth",
         "phonenumber",
