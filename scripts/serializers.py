0579e90810783b5ee34af04496a9aee9722ff650
yuunseo <dbstj7457@naver.com>
2023-07-28 17:57:13 +0900

    #serializer 설명 추가

--- scripts/serializers.py
+++ scripts/serializers.py
@@ -2,12 +2,14 @@ from rest_framework.serializers import ModelSerializer
 from .models import Script
 
 
+# detail화면에서 email을 간단히 보기 위한 serializer
 class TinyEmailSerializer(ModelSerializer):
     class Meta:
         model = Script
         fields = ("email",)
 
 
+# put요청할 때, 두가지 필드만 입력받기 위한 serializer
 class ScriptTinySerializer(ModelSerializer):
     class Meta:
         model = Script
@@ -17,12 +19,14 @@ class ScriptTinySerializer(ModelSerializer):
         )
 
 
+# 전체 scripts를 보기 위한 serializer
 class ScriptSerializer(ModelSerializer):
     class Meta:
         model = Script
         fields = "__all__"
 
 
+# datil script를 보기 위한 serializer
 class ScriptDetailSerializer(ModelSerializer):
     email = TinyEmailSerializer()
 
