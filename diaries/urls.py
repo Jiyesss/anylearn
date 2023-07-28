878afd052bd06c3b5aad8b5ee55df5df8d182abe
yuunseo <dbstj7457@naver.com>
2023-07-28 18:28:17 +0900

    #diary의 pk가 설정되어있지 않은데 무엇으로 경로접근?

--- diaries/urls.py
+++ diaries/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.Diaries.as_view()),
-    path("<int:pk>", views.Diaries.as_view()),
+    path("<slug:nowDate>", views.Diaries.as_view()),
 ]
