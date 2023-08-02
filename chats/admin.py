39353e0ad6af826019b4acef035b92bc67f52f0d
yuunseo <dbstj7457@naver.com>
2023-08-02 18:06:00 +0900

    #admin 페이지에 chats 모델 추가

--- chats/admin.py
+++ chats/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import RolePlayingRoom
 
-# Register your models here.
+
+@admin.register(RolePlayingRoom)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "level",
+        "situation",
+        "my_role",
+        "gpt_role",
+    ]
