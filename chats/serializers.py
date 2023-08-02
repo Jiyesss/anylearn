d737203e87f70deb1663ec620091b81c414c7843
yuunseo <dbstj7457@naver.com>
2023-08-02 21:23:23 +0900

    #POST 요청 시 번역 기능 성공

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import RolePlayingRoom
 from users.models import User
+from .translators import google_translate
 
 
 # detail chat에서 보고 싶은 field만 보기 위한 serializer
@@ -30,3 +32,43 @@ class ChatDetailSerializer(ModelSerializer):
         model = RolePlayingRoom
         fields = "__all__"
         depth = 1
+
+
+# 번역기능추가
+class RolePlayingRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RolePlayingRoom
+        fields = [
+            "level",
+            "situation",
+            "situation_en",
+            "my_role",
+            "my_role_en",
+            "gpt_role",
+            "gpt_role_en",
+        ]
+
+    def validate(self, data):
+        situation = data.get("situation")
+        situation_en = data.get("situation_en")
+        if situation and not situation_en:
+            data["situation_en"] = self._translate(situation)
+
+        my_role = data.get("my_role")
+        my_role_en = data.get("my_role_en")
+        if my_role and not my_role_en:
+            data["my_role_en"] = self._translate(my_role)
+
+        gpt_role = data.get("gpt_role")
+        gpt_role_en = data.get("gpt_role_en")
+        if gpt_role and not gpt_role_en:
+            data["gpt_role_en"] = self._translate(gpt_role)
+
+        return data
+
+    @staticmethod
+    def _translate(origin_text: str) -> str:
+        translated = google_translate(origin_text, "auto", "en")
+        if not translated:
+            raise serializers.ValidationError("구글 번역에 실패했습니다.")
+        return translated
--- chats/views.py
+++ chats/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.utils.decorators import method_decorator
+from django.contrib.admin.views.decorators import staff_member_required
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework import status
 from .models import RolePlayingRoom
-from .serializers import ChatSerializer, ChatDetailSerializer
+from .serializers import ChatSerializer, ChatDetailSerializer, RolePlayingRoomSerializer
+from gtts import gTTS
 
 
 # /api/v1/diaries url에 접근했을 때 API
 class Chats(APIView):
     def get(self, requet):
         all_chats = RolePlayingRoom.objects.all()
-        serializer = ChatSerializer(
+        serializer = RolePlayingRoomSerializer(
             all_chats,
             many=True,
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RolePlayingRoomSerializer(data=request.data)
+        if serializer.is_valid():
+            new_chat = serializer.save(user=request.user)
+            return Response(RolePlayingRoomSerializer(new_chat).data)
+        else:
+            return Response(serializer.errors)
+
+
+class RolePlayingRoomCreateView(generics.CreateAPIView):
+    queryset = RolePlayingRoom.objects.all()
+    serializer_class = RolePlayingRoomSerializer
 
 
 # /api/v1/chats/[pk] url에 접근했을 때 API
@@ -50,3 +68,15 @@ class ChatDetail(APIView):
         chat = self.get_object(pk)
         chat.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@staff_member_required
+def make_voice(request):
+    lang = request.GET.get("lang", "en")
+    message = request.GET.get("message")
+
+    response = Response()
+    gTTS(message, lang=lang).write_to_fp(response)
+    response["Content-Type"] = "audio/mpeg"
+
+    return response
