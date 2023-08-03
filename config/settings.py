3abc21554d9512fc0c1daf11a6b61f53013684f6
yuunseo <dbstj7457@naver.com>
2023-08-03 16:14:31 +0900

    websocket url경로 추가

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-ASGI_APPLICATION = "mysite.asgi.application"
+ASGI_APPLICATION = "config.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
