6625f59fc1fd9b00bcefa7fddcb12a29cd5aea5e
yuunseo <96402411+yuunseo@users.noreply.github.com>
2023-08-26 07:53:34 +0900

    allowed_host변경

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = "django-insecure-@5q1u($b^0#-gywryihv7_8s7ewcwal+e0&a5g%8gy=%lh5n-!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "35.236.157.81", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*", "35.234.62.122"]
 
 
 # Application definition
