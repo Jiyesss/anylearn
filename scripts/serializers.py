0de2de88940d08398837b166ea195c78a59148d7
yuunseo <dbstj7457@naver.com>
2023-08-31 07:23:30 +0900

    #18 - scripts put 요청 코드 추가

--- scripts/serializers.py
+++ scripts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from .models import Script, Tag
+from diaries.models import Diary
 
 
 # detail화면에서 email을 간단히 보기 위한 serializer
@@ -21,23 +22,53 @@ class ScriptTinySerializer(ModelSerializer):
 
     class Meta:
         model = Script
-        fields = ("hashtag", "contents")
+        fields = ("hashtag", "contents", "add_diary")
 
     def update(self, instance, validated_data):
-        # hashtag 데이터 추출 후 삭제
-        hashtag_data = validated_data.pop("hashtag", [])
+        if "hashtag" in validated_data:
+            # hashtag 데이터 추출 후 삭제
+            hashtag_data = validated_data.pop("hashtag", [])
 
-        # Update other fields as needed...
-        instance.contents = validated_data.get("contents", instance.contents)
-        instance.save()
+            # Update other fields as needed...
+            instance.contents = validated_data.get("contents", instance.contents)
+            instance.add_diary = validated_data.get("add_diary", instance.add_diary)
+            instance.save()
 
-        # Update the associated tags for the script
-        instance.hashtag.clear()
-        for tag_data in hashtag_data:
-            tag, created = Tag.objects.get_or_create(tag=tag_data["tag"])
-            instance.hashtag.add(tag)
+            # Update the associated tags for the script
+            if hashtag_data is not None:
+                instance.hashtag.clear()
+                for tag_data in hashtag_data:
+                    tag, created = Tag.objects.get_or_create(tag=tag_data["tag"])
+                    instance.hashtag.add(tag)
 
-        return instance
+            return instance
+
+        elif "contents" in validated_data:
+            # Update contents field
+            instance.contents = validated_data.get("contents", instance.contents)
+
+            # Update other fields as needed...
+            hashtag_data = validated_data.pop("hashtag", None)
+
+            if hashtag_data is not None:
+                instance.hashtag.clear()
+                for tag_data in hashtag_data:
+                    tag, created = Tag.objects.get_or_create(tag=tag_data["tag"])
+                    instance.hashtag.add(tag)
+            instance.add_diary = validated_data.get("add_diary", instance.add_diary)
+            instance.save()
+
+            return instance
+
+        elif "add_diary" in validated_data:
+            add_diary_data = 1
+
+            # Update other fields as needed...
+            instance.contents = validated_data.get("contents", instance.contents)
+            instance.hashtag = validated_data.get("hashtag", instance.hashtag)
+            instance.save()
+
+            created_diary = Diary.objects.get_or_create(diaryContents=instance.contents)
 
 
 # 전체 scripts를 보기 위한 serializer
